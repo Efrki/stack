d1bcf399315c7b3d59dd3f43703214d7737479c6
Kirya <efremenko.kirill2005@gmail.com>
2024-03-17 22:39:23 +0300

    Abvgd

---
+++ stack.py
@@ -0,0 +1,16 @@
+class Stack:
+    def __init__(self) -> None:
+        self.stack = []
+    
+    def push(self, item):
+        self.stack.append(item)
+    
+    def pop(self):
+        removed = self.stack.pop()
+        return removed
+        
+    def is_empty(self):
+        if len(self.stack) != 0:
+            return True
+        else:
+            return False
